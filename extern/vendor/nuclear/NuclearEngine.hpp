cbc0ddd81ed70c7e48fcb782f4e5d6ca4cf70b44
Hamdi Moubrak <104172250+ElCapor@users.noreply.github.com>
2024-05-09 21:32:29 +0200

    added

---
+++ extern/vendor/nuclear/NuclearEngine.hpp
@@ -0,0 +1,17 @@
+#ifndef NUCLEAR_ENGINE_HPP
+#define NUCLEAR_ENGINE_HPP
+/*
+* The main nuclear reactor engine code
+*/
+
+namespace nuclear
+{
+    /// @brief Nuclear Reactor Engine
+    class Engine
+    {
+
+    };
+}
+
+
+#endif
