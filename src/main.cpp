943d015e045ec69d70724606694ce74a21c410fd
Hamdi Moubrak <104172250+ElCapor@users.noreply.github.com>
2024-05-09 18:11:37 +0200

    Base project

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#define DOCTEST_CONFIG_IMPLEMENT
+#include "factorial.hpp"
+#include <iostream>
+int main()
+{
+    std::cout << factorial(2) << std::endl;
+    return 0;
+}
---
+++ xmake.lua
@@ -0,0 +1,33 @@
+set_allowedmodes("release", "debug")
+
+target("Reactor")
+    set_kind("binary")
+    set_languages("cxx23")
+    -- Source files
+    add_files("src/**.cpp")
+    add_files("src/**.c")
+    remove_files("src/test.cpp")
+    -- Header files
+    add_headerfiles("src/**.hpp")
+    add_headerfiles("src/**.h")
+
+    -- Include Directories
+    add_includedirs("extern/vendor")
+    add_includedirs("src")
+target_end()
+
+target("Test")
+        set_kind("binary")
+    set_languages("cxx23")
+    -- Source files
+    add_files("src/**.cpp")
+    add_files("src/**.c")
+    remove_files("src/main.cpp")
+    -- Header files
+    add_headerfiles("src/**.hpp")
+    add_headerfiles("src/**.h")
+
+    -- Include Directories
+    add_includedirs("extern/vendor")
+    add_includedirs("src")
+target_end()
