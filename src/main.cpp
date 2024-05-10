0fef47023330de19e1159675f42f55ffdd6770cd
Hamdi Moubrak <104172250+ElCapor@users.noreply.github.com>
2024-05-10 23:30:25 +0200

    use .concat() and not .append()

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,27 @@
 #include <nuclear/NuclearEngine.hpp>
 #include <luaconfig/luaconf.h>
 #include <filesystem>
+#include "utils/ResourceManager.hpp"
+
 namespace fs = std::filesystem;
 
-fs::path GetCurrentExecutablePath()
-{
+fs::path GetCurrentExecutablePath() {
     return fs::current_path();
 }
 
 int main()
 {
     nuclear::Engine* engine = new nuclear::Engine();
+    ResourceManager* rsrc = new ResourceManager();
+    std::cout << GetCurrentExecutablePath() << std::endl;
+    rsrc->ResourcesPath() = GetCurrentExecutablePath().concat("\\resources");
+    std::cout << rsrc->ResourcesPath() << std::endl;
+    std::cout << rsrc->ReadEngineConfig("reactorParameters.lua") << std::endl;
     luaconf::Value reactorConfig;
     //bool ret = luaconf::ParseFile(.string(),reactorConfig);
     auto exec = GetCurrentExecutablePath();
     auto data = exec.append("\\resources\\data");
     delete engine;
+    delete rsrc;
     return 0;
 }
--- src/utils/ResourceManager.hpp
+++ src/utils/ResourceManager.hpp
@@ -1,14 +1,18 @@
 #ifndef RESOURCEMANAGER_HPP
 #define RESOURCEMANAGER_HPP
 #include <filesystem>
+#include "FilesystemUtils.hpp"
 namespace fs = std::filesystem;
 class ResourceManager
 {
 public:
+
     fs::path& ResourcesPath() {return m_ResourcesPath;}
-    fs::path& ConfigPath() {return m_ResourcesPath.append("\\config");}
-    fs::path& EngineConfigPath() {return ConfigPath().append("\\engine");}
-    fs::path& DataPath() {return m_ResourcesPath.append("\\data");}
+    fs::path& ConfigPath() {return m_ResourcesPath.concat("\\config");}
+    fs::path& EngineConfigPath() {return ConfigPath().concat("\\engine");}
+    fs::path& DataPath() {return m_ResourcesPath.concat("\\data");}
+
+    std::string ReadEngineConfig(std::string m_name) {return fsutils::loadFile(EngineConfigPath().concat("\\" + m_name).string());};
 private:
     fs::path m_ResourcesPath;
 };
